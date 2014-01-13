7851410f84a70d2de6b2aa9151bb450be1fe86a6
Hunt Burdick <hunt@impulsedevelopment.com>
2014-01-13 15:36:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CmsAdmin2::Application.config.secret_key_base = 'b733afb9aac5e2f22cb1b47da71e4eb145f42fe6303006c41389d938535553109d706b5feb908bf2de02346ba49facd8081954c8850a96f22a156754d4e2dfae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CmsAdmin2::Application.config.session_store :cookie_store, key: '_cms_admin2_session'
