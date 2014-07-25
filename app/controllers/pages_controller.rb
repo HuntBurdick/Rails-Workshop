e7b9b760b95716b9570c87078b632a2f1b78572e
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-07-25 10:24:08 -0600

    Custom page modules and controller resources.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,19 @@ class PagesController < ApplicationController
   def show
     @page = Page.friendly.find(params[:id])
     @posts = Post.where(:page_id => @page.id, :published => true).order("position ASC")
+
+    send(@page.name.parameterize)
+    # home
+
     respond_to do |format|
       format.html
     end
   end
+
+  private
+    def home
+      # Resources for page modules can be set here.
+      
+    end
 
 end
